a288384878277bb2e1f48d0e01b8e0e02326ee67
joycie416 <joycie416@gmail.com>
2024-07-24 11:54:02 +0900

    q4 switch를 이용한 답 추가

--- 240724/q4.js
+++ 240724/q4.js
@@ -1,14 +1,31 @@
 function operate(num1, operator, num2) {
   if (operator === '+') {
-    return(num1 + num2);
-  }else if (operator === '-') {
-    return(num1 - num2);
-  }else if (operator === '*') {
-    return(num1 * num2);
-  }else if (operator === '/') {
-    return(num1 / num2);
+    return (num1 + num2);
+  } else if (operator === '-') {
+    return (num1 - num2);
+  } else if (operator === '*') {
+    return (num1 * num2);
+  } else if (operator === '/') {
+    return (num1 / num2);
   }
 }
+
+
+function operate2(num1, operator, num2) {
+  switch (operator) {
+    case '+':
+      return (num1 + num2);
+    case '-':
+      return (num1 - num2);
+    case '*':
+      return (num1 * num2);
+    case '/':
+      return (num1 / num2);
+    default:
+      return null;
+  }
+}
+
 
 console.log(operate(3, "+", 6)); // 결과값 9
 console.log(operate(11, "-", 6)); // 결과값 5
