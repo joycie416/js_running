46d92be5c0f3b46b8cf5d6b0d6b51f77541eed9f
joycie416 <joycie416@gmail.com>
2024-07-26 12:12:42 +0900

    q6 method chaining? 추가

--- 240726/q6.js
+++ 240726/q6.js
@@ -15,6 +15,16 @@ let good = students.filter(student => {
 console.log(good);
 
 
+// method chaining?
+let good1 = students.filter(student => {
+  let mean = student.scores.reduce((sum,cur) => sum + cur)/student.scores.length;
+  return mean >= 80;
+})
+good1 = good1.map(student => student.name);
+console.log(good1);
+
+
+
 // 2.
 const numbers = [5, 10, 15, 20, 25];
 // 요소에 2를 곱하고, 30 이하인 것들만 반환받아 출력해주세요
@@ -22,3 +32,8 @@ const numbers = [5, 10, 15, 20, 25];
 let newNumbers = numbers.map(num => num*2);
 newNumbers = newNumbers.filter(num => num <= 30);
 console.log(newNumbers);
+
+
+// method chaining?
+let newNumbers1 = numbers.map(num => num*2).filter(num => num <= 30);
+console.log(newNumbers1);
