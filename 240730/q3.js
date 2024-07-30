682cf1fbab04a33294e32b3c202a3861171eb315
joycie416 <joycie416@gmail.com>
2024-07-30 11:34:29 +0900

    q3 solved

---
+++ 240730/q3.js
@@ -0,0 +1,59 @@
+function task1() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Task 1 Complete");
+    }, 1000);
+  });
+}
+
+function task2() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Task 2 Complete");
+    }, 2000);
+  });
+}
+
+function task3() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Task 3 Complete");
+    }, 3000);
+  });
+}
+
+async function runTasksSequential() {
+  console.time("Sequential");
+  try {
+    const result1 = await task1();
+    console.log(result1);
+    const result2 = await task2();
+    console.log(result2);
+    const result3 = await task3();
+    console.log(result3);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  console.timeEnd("Sequential");
+
+  // 예상되는 대략적인 시간과 그 이유 < 빈칸 1 >
+  // 6초 : await에 의해 완료될 때까지 기다렸다가 차례로 진행됨
+}
+
+async function runTasksParallel() {
+  console.time("Parallel");
+  try {
+    // 병렬로 promise들을 한 번에 처리하는 코드
+    const results = Promise.all([task1, task2, task3]); // < 빈칸 2 >
+    results.forEach((result) => console.log(result));
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  console.timeEnd("Parallel");
+  // 예상되는 대략적인 시간과 그 이유 < 빈칸 3 >
+  // 한번에 여러 비동기 함수를 실행하므로 가장 오래 걸리는 비동기 함수가 끝난 후 결과가 나옴
+  // 즉 3초.
+}
+
+// 함수 호출
+runTasksSequential().then(() => runTasksParallel());
